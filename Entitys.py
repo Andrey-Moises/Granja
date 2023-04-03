65a60d1a6ddf92fa938f7ccbe53b79111c7171af
andrey <andreymoises.garcia@gmail.com>
2023-04-03 16:10:47 -0600

    Movimientos basicos y funcionalidades

--- Entitys.py
+++ Entitys.py
@@ -1,4 +1,47 @@
-class Entity:
+import random
 
-    def __int__(self, posisition):
+
+class Arbusto:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Oveja:
+    def __init__(self, x, y, map):
+        self.pastPostX = 0
+        self.pastPostY = 0
+        self.axisX = x
+        self.axisY = y
+        self.figure = "O"
+        self.map = map
+        self.knwlg = [[]]
+
+    def move(self):
+        self.pastPostX = self.axisX
+        self.pastPostY = self.axisY
+        self.axisX += 1
+        self.axisY += 1
+
+
+class Pastor:
+    def __init__(self, x, y, map):
         pass
+
+    def mover(self):
+        pass
+
+
+def existinmap(entity, axisx, axisy, map):
+    map[axisx][axisy] = entity
+
+
+def renewposition(axisx, axisy, map):
+    map[axisx][axisy] = ' '
+
+
+def printmap(map):
+    for fila in map:
+        for pos in fila:
+            print(pos, end=" ")
+        print()
